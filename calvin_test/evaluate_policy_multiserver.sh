e2add44ffc430342e78c6c1d9a27f76f6048d6f6
你的GitHub用户名 <haoangli@mgmt-4.hkust-gz.edu.cn>
2025-06-16 15:05:02 +0800

    add calvin test
    >

---
+++ calvin_test/evaluate_policy_multiserver.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+## activate the calvin environment
+conda activate calvin_venv
+## choose the gpu
+export EGL_VISIBLE_DEVICES=0
+trap "kill 0" EXIT
+
+
+## set the port list , the evaluation sequence is split into chunks, each chunk is evaluated by a server
+PORTSLIST=(9097 9098)
+SERVER_IP="127.0.0.1"
+EVAL_LOG_DIR="./calvin_test/evaluation/log"
+CHUNKS=${#PORTSLIST[@]}
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    python ./calvin_test/evaluate_policy_multiserver.py \
+        --dataset_path /share/user/iperror/data/task_ABC_D \
+        --question_file  /data/user/wsong890/user68/project/calvin/calvin_models/calvin_agent/evaluation/evaluation_sequence/questions/question.json\
+        --eval_log_dir $EVAL_LOG_DIR \
+        --num_chunks $CHUNKS \
+        --chunk_idx $IDX \
+        --port ${PORTSLIST[$IDX]} \
+        --server_ip $SERVER_IP \
+        --save_dir /data/user/wsong890/user68/project/rossvla/debug_video \
+        --action_chunk 5 \
+        --conf_dir /data/user/wsong890/user68/project/calvin/calvin_models/conf \
+        --custom_model &
+done
+        # --debug \
+#save_name: 测试文件结果的名称的前缀，后缀自动为运行时间
+#save_dir: 在debug模式下，测试视频结果的保存路径
+#如果模型端和服务器端不在一台机器上，要设置一下网口位置
+
+
+wait
