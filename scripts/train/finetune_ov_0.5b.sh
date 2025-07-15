ffc9c47b861cbbd6af0e23fb6bbe8b26d84b8ab1
chenpy <102129825+chris1220313648@users.noreply.github.com>
2025-07-15 20:46:11 +0800

    Update finetune_ov_0.5b.sh

--- scripts/train/finetune_ov_0.5b.sh
+++ scripts/train/finetune_ov_0.5b.sh
@@ -49,7 +49,7 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node=2 --nnodes=1 --node_rank=0 -
     --run_name $RUN_NAME \
     --output_dir $OUTPUT_DIR \
     --num_train_epochs 5 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
