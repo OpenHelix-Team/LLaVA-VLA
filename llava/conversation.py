b000b0a75d68a5b15031ec3d1964f2dc771cd77e
chenpy <102129825+chris1220313648@users.noreply.github.com>
2025-07-19 19:19:40 +0800

    Update conversation.py

--- llava/conversation.py
+++ llava/conversation.py
@@ -563,7 +563,7 @@ Answer the questions.""",
     sep="<|im_end|>",
 )
 
-default_conversation = conv_vicuna_v0
+default_conversation = conv_vicuna_v1 
 conv_templates = {
     "default": conv_vicuna_v0,
     "v0": conv_vicuna_v0,
